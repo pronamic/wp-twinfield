30d30837404cf2a2cb9a55bb485cf4e74506e9ae
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-05-30 11:03:07 +0200

    Add Twinfield icon.

---
+++ admin/page-dashboard.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Page dashboard
+ *
+ * @author Pronamic <info@pronamic.eu>
+ * @copyright 2005-2022 Pronamic
+ * @package Pronamic/WordPress/Twinfield
+ */
+
+?>
+<div class="wrap">
+	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+
+</div>
--- src/Plugin/Admin.php
+++ src/Plugin/Admin.php
@@ -45,16 +45,83 @@ class Admin {
 	}
 
 	/**
+	 * Get menu icon URL.
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_menu_page/
+	 * @return string
+	 * @throws \Exception Throws exception when retrieving menu icon fails.
+	 */
+	private function get_menu_icon_url() {
+		/**
+		 * Icon URL.
+		 *
+		 * Pass a base64-encoded SVG using a data URI, which will be colored to match the color scheme.
+		 * This should begin with 'data:image/svg+xml;base64,'.
+		 *
+		 * We use a SVG image with default fill color #A0A5AA from the default admin color scheme:
+		 * https://github.com/WordPress/WordPress/blob/5.2/wp-includes/general-template.php#L4135-L4145
+		 *
+		 * The advantage of this is that users with the default admin color scheme do not see the repaint:
+		 * https://github.com/WordPress/WordPress/blob/5.2/wp-admin/js/svg-painter.js
+		 *
+		 * @link https://developer.wordpress.org/reference/functions/add_menu_page/
+		 */
+		$file = __DIR__ . '/../../images/dist/wp-twinfield-wp-admin-fresh-base.svgo-min.svg';
+
+		if ( ! \is_readable( $file ) ) {
+			throw new \Exception(
+				\sprintf(
+					'Could not read WordPress admin menu icon from file: %s.',
+					$file
+				)
+			);
+		}
+
+		$svg = \file_get_contents( $file, true );
+
+		if ( false === $svg ) {
+			throw new \Exception(
+				\sprintf(
+					'Could not read WordPress admin menu icon from file: %s.',
+					$file
+				)
+			);
+		}
+
+		$icon_url = \sprintf(
+			'data:image/svg+xml;base64,%s',
+			// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
+			\base64_encode( $svg )
+		);
+
+		return $icon_url;
+	}
+
+	/**
 	 * Admin menu.
 	 */
 	public function admin_menu() {
+		try {
+			$menu_icon_url = $this->get_menu_icon_url();
+		} catch ( \Exception $e ) {
+			/**
+			 * If retrieving the menu icon URL fails we will
+			 * fallback to the WordPress site dashicon.
+			 *
+			 * @link https://developer.wordpress.org/resource/dashicons/#admin-site-alt3
+			 */
+			$menu_icon_url = 'dashicons-admin-site-alt3';
+		}
+
 		\add_menu_page(
 			\__( 'Twinfield', 'pronamic-twinfield' ),
 			\__( 'Twinfield', 'pronamic-twinfield' ),
 			'manage_options',
 			'pronamic-twinfield',
-			[ $this, 'page_dashboard' ],
-			'dashicons-admin-site-alt3'
+			function() {
+				include __DIR__ . '/../../admin/page-dashboard.php';
+			},
+			$menu_icon_url
 		);
 
 		\add_submenu_page(
