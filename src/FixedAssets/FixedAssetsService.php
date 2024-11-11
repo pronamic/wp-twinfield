8827bb72804f5be9b8e9eeaae620d1b4732c300e
Remco Tolsma <remcotolsma@pronamic.nl>
2024-11-11 18:58:01 +0100

    Fixed assets service.

---
+++ src/FixedAssets/FixedAssetsService.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Fixed assets service
+ *
+ * @package Pronamic/WordPress/Twinfield
+ */
+
+namespace Pronamic\WordPress\Twinfield\Documents;
+
+use Pronamic\WordPress\Twinfield\AbstractService;
+use Pronamic\WordPress\Twinfield\Client;
+
+/**
+ * Fixed assets service class
+ */
+class FixedAssetsService {
+	/**
+	 * Get assets.
+	 * 
+	 * @return array
+	 */
+	public function get_assets() {
+
+	}
+}
