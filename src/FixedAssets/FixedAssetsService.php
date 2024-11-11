fbeef700d977a7d13805f5bd0d1d8449c83034f5
Remco Tolsma <remcotolsma@pronamic.nl>
2024-11-11 19:10:48 +0100

    Update FixedAssetsService.php

--- src/FixedAssets/FixedAssetsService.php
+++ src/FixedAssets/FixedAssetsService.php
@@ -17,9 +17,12 @@ class FixedAssetsService {
 	/**
 	 * Get assets.
 	 * 
+	 * @link https://api.accounting.twinfield.com/Api/swagger/ui/index#/
+	 * @param string $organisation_id Organisation ID.
+	 * @param string $company_id      Company ID.
 	 * @return array
 	 */
-	public function get_assets() {
-
+	public function get_assets( $organisation_id, $company_id ) {
+		
 	}
 }
