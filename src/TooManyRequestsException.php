b48c9b84aeac83b8c1d9365ad5e0c6cf564b341c
Remco Tolsma <remcotolsma@pronamic.nl>
2025-02-24 11:17:31 +0100

    Add more details to exception message.

--- src/TooManyRequestsException.php
+++ src/TooManyRequestsException.php
@@ -11,6 +11,7 @@ namespace Pronamic\WordPress\Twinfield;
  * Too Many Requests Exception class
  * 
  * @link https://docs.saloon.dev/installable-plugins/handling-rate-limits#handling-rate-limits-being-exceeded
+ * @link https://www.php.net/manual/en/class.runtimeexception.php
  */
 class TooManyRequestsException extends \RuntimeException {
 	/**
@@ -19,11 +20,15 @@ class TooManyRequestsException extends \RuntimeException {
 	 * @param \SoapFault $soap_fault SOAP fault.
 	 * @return self
 	 */
-	public function from_soap_client_fault( SoapClient $soap_client, \SoapFault $soap_fault ) {
+	public static function from_soap_client_fault( SoapClient $soap_client, \SoapFault $soap_fault ) {
 		$message = \sprintf(
-			'SOAP Fault: %s' . "\n" .
+			'SOAP Fault Code: %s' . "\n" .
+			'SOAP Fault Message: %s' . "\n" .
+			'SOAP Fault JSON: %s' . "\n" .
 			'SOAP Response Headers: %s',
+			$soap_fault->getCode(),
 			$soap_fault->getMessage(),
+			\wp_json_encode( $soap_fault ),
 			$soap_client->__getLastResponseHeaders()
 		);
 
