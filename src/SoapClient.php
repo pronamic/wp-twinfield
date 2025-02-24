d152a9e7238d70f08ab75cc7d40c0bca24ea8fda
Remco Tolsma <remcotolsma@pronamic.nl>
2025-02-24 11:18:08 +0100

    Also handle WSDL retrieving exceptions.

--- src/SoapClient.php
+++ src/SoapClient.php
@@ -12,6 +12,21 @@ namespace Pronamic\WordPress\Twinfield;
  */
 class SoapClient extends \SoapClient {
 	/**
+	 * Construct SOAP client.
+	 * 
+	 * @link https://www.php.net/manual/en/soapclient.construct.php
+	 * @param ?string $wsdl URI of a WSDL file describing the service, which is used to automatically configure the client. If not provided, the client will operate in non-WSDL mode.
+	 * @param array   $options An associative array specifying additional options for the SOAP client. If `wsdl` is provided, this is optional; otherwise, at least location and url must be provided.
+	 */
+	public function __construct( ?string $wsdl, array $options = [] ) {
+		try {
+			return parent::__construct( $wsdl, $options );
+		} catch ( \SoapFault $soap_fault ) {
+			$this->handle_soap_fault( $soap_fault );
+		}
+	}
+
+	/**
 	 * Performs a SOAP request.
 	 * 
 	 * @link https://www.php.net/manual/en/soapclient.dorequest.php
@@ -26,12 +41,23 @@ class SoapClient extends \SoapClient {
 		try {
 			return parent::__doRequest( $request, $location, $action, $version, $one_way );
 		} catch ( \SoapFault $soap_fault ) {
-			if ( $this->is_too_many_requests( $soap_fault ) ) {
-				throw TooManyRequestsException::from_soap_client_fault( $this, $soap_fault );
-			}
-
-			throw $soap_fault;
+			$this->handle_soap_fault( $soap_fault );
 		}
+	}
+
+	/**
+	 * Handles a SOAP fault exception.
+	 *
+	 * @param \SoapFault $soap_fault The SOAP fault exception.
+	 * @throws TooManyRequestsException If the error is due to rate limiting.
+	 * @throws \SoapFault If another SOAP error occurs.
+	 */
+	public function handle_soap_fault( \SoapFault $soap_fault ): void {
+		if ( $this->is_too_many_requests( $soap_fault ) ) {
+			throw TooManyRequestsException::from_soap_client_fault( $this, $soap_fault );
+		}
+
+		throw $soap_fault;
 	}
 
 	/**
